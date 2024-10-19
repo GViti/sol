2c55d0b6238452a68032274bc06530c7159cf62a
GViti <vitigabriele1@gmail.com>
2024-10-19 12:07:11 +0200

    modifica package

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',  // Aggiungi il percorso di base per gestire le risorse relative
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Specifica la directory di output della build
-  },
 })
